b45cac615fcbc6ca406db8f96c3567d41477b09d
Shipix <shipix@hotmail.fr>
2021-03-25 10:39:58 +0100

    Pt 3

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<form action="" method="post">
+    <label for="mail">Contactez-nous</label>
+    <textarea name="mail" id="" cols="30" rows="10">
+    </textarea>
+    <button type="submit">Envoyer</button>
+</form>
